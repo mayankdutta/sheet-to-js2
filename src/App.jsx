4e296213c11f90bb9292c3de9966d9b49091dcd3
mayank dutta <smayankdutt@gmail.com>
2023-01-07 03:36:24 +0530

    data arriving

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,44 @@ import Papa from "papaparse";
 
 import "./App.css";
 
-const URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2wDEs_PO6zvacvwEQ2XVh_CK5k5fZrXkng4W26Q0BO0EcR1EPzFj_yBleDq3ipmcU4_c_95XfIWZ9/pub?gid=547306195&single=true&output=csv";
+const SEARCH_ENGINE_DATA_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2wDEs_PO6zvacvwEQ2XVh_CK5k5fZrXkng4W26Q0BO0EcR1EPzFj_yBleDq3ipmcU4_c_95XfIWZ9/pub?gid=1633805794&single=true&output=csv";
+
+const Headers = [{}];
 
 function App() {
   const [tableData, setTableData] = useState([]);
-  // const [tableHeadRow, setTableHeadRow] = useState([]);
+  const [tableHeadRow, setTableHeadRow] = useState([]);
 
   useEffect(() => {
-    Papa.parse(URL, {
+    Papa.parse(SEARCH_ENGINE_DATA_URL, {
       download: true,
       // header: true,
 
+      before: () => {
+        setTableHeadRow([]);
+        setTableData([]);
+      },
       complete: (results) => {
-        setTableData(
-          results.data.filter((data) => {
-            let flag = false;
-
-            for (const index in data) {
-              flag = flag || data[index] != "";
-            }
-
-            // Object.keys(data).forEach((value) => (flag = flag && value != ""));
-            console.log(flag);
-            return flag;
-          })
-        );
-        console.log(results.data);
-
-        let headers = [];
-        for (let j in results.data[0]) {
-          headers.push(j);
+        for (let i in results.data[0]) {
+          setTableHeadRow((prev) => [...prev, results.data[0][i]]);
         }
-        // setTableHeadRow(headers);
+
+        results.data.shift();
+        results.data.shift();
+
+        results.data = results.data.filter((data) => {
+          let flag = false;
+          for (const index in data) {
+            if (data[index].length > 0) {
+              flag = true;
+            }
+          }
+          return flag;
+        });
+
+        console.log(tableHeadRow);
+        setTableData(results.data);
       },
     });
   }, []);
@@ -53,6 +58,17 @@ function App() {
             width: "100%",
           }}
         >
+          <thead>
+            <tr>
+              {tableHeadRow.map((value, index) => {
+                return (
+                  <th style={{ border: "2px solid black" }} key={index}>
+                    {value}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
           <tbody>
             {tableData.map((value, index) => (
               <tr key={index}>
